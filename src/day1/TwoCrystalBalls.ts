e8ca7895ffdd5395e0b45e571582c7b78d2ffce1
Martin Deyanov <martin_deianov@yahoo.com>
2025-06-24 18:32:32 +0000

    Tue, Jun 24, 2025, 9:32 PM +03:00

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,8 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jmpAmont = Math.floor(Math.sqrt(breaks.lenght));
+
+    for (let i = 0; i < breaks.length; i += jmpAmount) {
+        
+    }
 }
