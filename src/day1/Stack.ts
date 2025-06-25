a166342c4d7e2a9ec37aa11c31703c241a60fb29
Martin Deyanov <martin_deianov@yahoo.com>
2025-06-25 23:11:52 +0000

    Thu, Jun 26, 2025, 2:11 AM +03:00

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -17,11 +17,23 @@ export default class Stack<T> {
 
         this.length++;
         if (!this.head) {
-            this.head =
+            this.head = node;
+            return;
         }
+
+        node.prev = this.head;
+        this.head = node;
     }
 
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head as Node<T>;
+            this.head = undefined;
+            return head.value;
+        }
+
+        const head = this.head as Node<T>;
 
     }
     peek(): T | undefined {
