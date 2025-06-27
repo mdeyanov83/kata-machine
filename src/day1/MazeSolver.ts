bc9d2d4ca249e943daa2eaf782b291f57eb87831
Martin Deyanov <martin_deianov@yahoo.com>
2025-06-27 17:15:45 +0000

    Fri, Jun 27, 2025, 8:15 PM +03:00

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,7 +1,28 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
     // 1. Base Case
     // off the map
-    if (curr.x < 0 || curr.x >= maze[0].length)
+    if (curr.x < 0 || curr.x >= maze[0].length ||
+        curr.y < 0 || curr.y >+ maze.length) {
+        return false;
+    }
+
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // reached the end
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // if seen
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+
+
 }
 
 
