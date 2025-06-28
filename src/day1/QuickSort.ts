f67eed8bcb4bf28dd81cccea4270af6e45eae64e
Martin Deyanov <martin_deianov@yahoo.com>
2025-06-28 11:10:45 +0000

    Sat, Jun 28, 2025, 2:10 PM +03:00

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,13 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
 
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+
+    const pivot = arr[hi];
+    let idx = -1 
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length -1);
 }
