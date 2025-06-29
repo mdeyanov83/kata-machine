e7c9fd1451e88fde37626200b1c8a43b860779a8
Martin Deyanov <martin_deianov@yahoo.com>
2025-06-29 12:03:33 +0000

    Sun, Jun 29, 2025, 3:03 PM +03:00

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -45,8 +45,8 @@ export default class DoublyLinkedList<T> {
         node.next = curr;
         node.prev = curr.prev;
         curr.prev = node;
-        if (curr.prev) {
-            curr.prev.next = curr;
+        if (node.prev) {
+            node.prev.next = curr;
         }
     }
 
