532d8bfdd4a722220e268f422f4e3ab811d7bec6
Martin Deyanov <martin_deianov@yahoo.com>
2025-07-02 13:07:03 +0000

    Wed, Jul 2, 2025, 4:07 PM +03:00

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,8 +1,19 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
 
+    graph: WeightedAdjacencyMatrix;
+    source: number;
+    needle: number: number[] | null { }
+
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+
+    seen[source] = true;
+    const q: number[] = [source];
+
+    
     do {
 
-    } whiile 
+    } whiile
 
 
 
