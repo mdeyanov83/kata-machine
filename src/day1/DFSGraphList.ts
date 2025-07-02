d43958d50c2a57abaf2efc7689313fd14b404ab9
Martin Deyanov <martin_deianov@yahoo.com>
2025-07-02 15:07:12 +0000

    Wed, Jul 2, 2025, 6:07 PM +03:00

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,8 +1,37 @@
+function walk(Graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[]): boolean {
+
+    if (curr === needle) {
+        return true;
+    }
+    if (seen[curr]) {
+        return false;
+    }
+    seen[curr] = true;
+
+    // recurse
+    // pre
+    path.push(curr);
+
+    // recurse
+    const list = graph[curr];
+
+
+    // post
+    path.pop();
+
+
+}
+
+
 export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
     needle: number): number[] | null {
 
-    
+
 
 }
