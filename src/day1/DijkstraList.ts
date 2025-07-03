1a0100cc6b05679b46e0fbf9838f3d40e4438aae
Martin Deyanov <martin_deianov@yahoo.com>
2025-07-03 13:43:33 +0000

    Thu, Jul 3, 2025, 4:43 PM +03:00

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -17,23 +17,43 @@ function getLowestUnvisited(seen: boolean[], dists: number[]): number {
     return idx;
 }
 
-
 export default function dijkstra_list(
     source: number,
     sink: number,
     arr: WeightedAdjacencyList): number[] {
 
-
-
     const seen = new Array(arr.length).fill(false);
-    const dist = new Array(arr.length).fill(Infinity);
+    const prev = new Array(arr.length).fill(-1);
+    const dists = new Array(arr.length).fill(Infinity);
     dists[source] = 0;
 
     while (hasUnivisited(seen, dists)) {
         const curr = getLowestUnvisited(seen, dists);
+        seen[curr] = true;
 
-        
+        const adjs = arr[curr];
+        for (let i = 0; i < adjs.length; ++i) {
+            const edge = adjs[i];
+            if (seen[edge.to]) {
+                continue;
+            }
+            const dist = dists[curr] + edge.weight;
+            if (dist < dists[edge.to]) {
+                dists[edge.to] = dist;
+                prev[edge.to] = curr;
+            }
+        }
     }
 
+    const out: number[] = [];
+    let curr = sink;
+
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    out.push(source)
+    return out.reverse();
 }
 
