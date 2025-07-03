5f7075442437fb8af45df7856f298c3f1510b347
Martin Deyanov <martin_deianov@yahoo.com>
2025-07-03 13:08:17 +0000

    Thu, Jul 3, 2025, 4:08 PM +03:00

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,3 +1,11 @@
+function hasUnivisited(seen: boolean[], dists: number[]): boolean {
+    return seen.some((s, i) => !s && dists[i] < Infinity);
+}
+
+function getLowestUnvisited(seen: boolean[], dists: number[]): number {
+
+}
+
 
 export default function dijkstra_list(
     source: number,
@@ -6,6 +14,13 @@ export default function dijkstra_list(
 
 
 
+    const seen = new Array(arr.length).fill(false);
+    const dist = new Array(arr.length).fill(Infinity);
+    dists[source] = 0;
+
+    while (hasUnivisited(seen, dists)) {
+        const curr = getLowestUnvisited(seen, dists);
+    }
 
 }
 
