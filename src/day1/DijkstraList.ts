c2c0d4e496e71303f01f9ae66c0da040b719a6ec
Martin Deyanov <martin_deianov@yahoo.com>
2025-07-03 13:11:44 +0000

    Thu, Jul 3, 2025, 4:11 PM +03:00

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -3,7 +3,18 @@ function hasUnivisited(seen: boolean[], dists: number[]): boolean {
 }
 
 function getLowestUnvisited(seen: boolean[], dists: number[]): number {
-
+    let idx = -1;
+    let lowestDistance = Infinity;
+    for (let i = 0; i < seen.length; ++i) {
+        if (seen[i]) {
+            continue;
+        }
+        if (lowestDistance > dists[i]) {
+            lowestDistance = dists[i];
+            idx = i;
+        }
+    }
+    return idx;
 }
 
 
